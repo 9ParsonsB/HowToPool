b53ef9e8f4c54c83924cadf9e2a2ffd21089b086
minijack <ben.parsons@hotmail.co.uk>
2014-09-29 15:21:27 +0100

    added Types.cs; a list of all of mour data types. added Config.cs; will store getters / setters

---
+++ HowToPool/HowToPool/Types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HowToPool
+{
+    class Player
+    {
+        public string name; //TODO: give use chance to input name
+        public Entity Entity;
+        public int BallsRemaining;
+        public string colour;
+        public bool isTurn;
+    }
+    
+    class Entity
+    {
+
+    }
+}
