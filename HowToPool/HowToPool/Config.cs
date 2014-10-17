df2aacb2b225359c486b723d04d3e2941501f5f0
minijack <9parsonsb@gmail.com>
2014-10-17 18:11:33 +0100

    added for future settings menu:  -fov  -sale  -sound  -state (whenever changed set selected to 0)

--- HowToPool/HowToPool/Config.cs
+++ HowToPool/HowToPool/Config.cs
@@ -9,16 +9,26 @@ namespace HowToPool
     {
         private static State currentState = new State();
         public static List<Entity> Entities { get; set; }
-        public static string State { get; set; }
+        public static int maxFPS { get; set; }
         public static int Selected { get; set; }
+        public static int fov { get; set; }
         public static bool shouldCollide { get; set; }
         public static bool shouldResist { get; set; }
+        public static bool SALE { get; set; }
+        public static bool soundEnabled { get; set; }
         public static List<Ball> balls { get; set; }
 
         private static float _resistance = 0.05f; //usually 0.05f
 
         private static float _maxVel = 50f;
 
+        private static string _State;
+
+        public static string State
+        {
+            get { return _State; }
+            set { Selected = 0; _State = value; }
+        }
 
         public static float resistnace
         {
